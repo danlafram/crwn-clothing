f6de315aa5441407e8ca5240c8f96e71a1020cb9
Dan Laframboise <daniel.laframboise@youi.tv>
2020-11-30 08:32:32 -0500

    Switching computers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
